2e7959e68a6ced75ad55c5964960514982f4e7a6
AariyJP <aariyjp@aariy.net>
2021-05-02 20:01:56 +0900

    Presencer

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ RPC = Presence(739528267039768647)
 RPC.connect()
 start_time = int(time.time())
 while True:
-    RPC.update(start=start_time, large_image="youtube", join="abc")
+    # RPC.update(start=start_time, large_image="youtube", join="abc")  # YouTube
+    RPC.update(start=start_time, large_image="prime_video", join="abc")  # Prime Video
     time.sleep(60)
