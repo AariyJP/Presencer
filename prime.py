9827e3d3b3ad5c0fcb8b5f5ab2994dc6f2f6ca05
AariyJP <aariyjp@aariy.net>
2021-06-20 02:24:21 +0900

    Presencer

--- prime.py
+++ prime.py
@@ -5,5 +5,5 @@ PRIME = Presence(838237077602566166)
 PRIME.connect()
 start_time = int(time.time())
 while True:
-    PRIME.update(start=start_time, large_image="prime_video", join="abc")  # Prime Video
+    PRIME.update(details=None, start=start_time, large_image="prime_video", join="abc")  # Prime Video
     time.sleep(30)
