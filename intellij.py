da64357400e5eb5af1c0e65f5a0d8bbb485bef00
AariyJP <aariyjp@aariy.net>
2021-06-22 23:20:49 +0900

    Presencer

--- intellij.py
+++
@@ -1,9 +0,0 @@
-from pypresence import Presence
-import time
-
-INTELLIJ = Presence(626050213651677214)
-INTELLIJ.connect()
-start_time = int(time.time())
-while True:
-    INTELLIJ.update(start=start_time, large_image="java", join="abc")
-    time.sleep(30)
---
+++ study.py
@@ -0,0 +1,9 @@
+from pypresence import Presence
+import time
+
+STUDY = Presence(739528267039768647)
+STUDY.connect()
+start_time = int(time.time())
+while True:
+    STUDY.update(start=start_time, join="abc", buttons=[{"label": "Aariy.NETに参加", "url": "https://discord.com/invite/jeghwmr"}, {"label": "ANEボット導入", "url": "https://discord.com/oauth2/authorize?client_id=482195859186909184&scope=bot+applications.commands&permissions=2147483647"}])
+    time.sleep(30)
