98e1920c84bff51f53f45f52c5baf947fe1a4c86
AariyJP <aariyjp@aariy.net>
2021-06-23 23:26:29 +0900

    Presencer

--- study.py
+++ study.py
@@ -3,6 +3,7 @@ import time
 
 STUDY = Presence(739528267039768647)
 STUDY.connect()
+print(f"[pypresence]: Successfully connected to Discord client.")
 start_time = int(time.time())
 while True:
     STUDY.update(start=start_time, buttons=[{"label": "Aariy.NETに参加", "url": "https://discord.com/invite/jeghwmr"}, {"label": "ANEボット導入", "url": "https://discord.com/oauth2/authorize?client_id=482195859186909184&scope=bot+applications.commands&permissions=2147483647"}])
