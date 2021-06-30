2a5e317b2a2f88140b8af75e94191b885435b075
AariyJP <aariyjp@aariy.net>
2021-06-30 12:51:24 +0900

    Presencer

--- aariynet.py
+++ aariynet.py
@@ -3,6 +3,7 @@ import time
 
 AARIYNET = Presence(740833703277887491)
 AARIYNET.connect()
+print(f"[pypresence]: Successfully connected to Discord client.")
 start_time = int(time.time())
 while True:
     AARIYNET.update(start=start_time, large_image="aariyjp", buttons=[{"label": "Aariy.NETに参加", "url": "https://discord.com/invite/jeghwmr"}, {"label": "ANEボット導入", "url": "https://discord.com/oauth2/authorize?client_id=482195859186909184&scope=bot+applications.commands&permissions=2147483647"}])  # Prime Video
